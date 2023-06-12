e70ee414a9623b7f996cf290bb474f1354ea2350
Vitor <santosgomesv@gmail.com>
2023-06-11 22:00:48 -0300

    #V 2.0 ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -235,3 +235,9 @@
 })()
 
 
+function botao(){
+  document.getElementById('frameVideo').hidden =false;
+  var video = document.getElementById('frameVideo');
+  video.autoplay= true;
+  video.load(); 
+}
